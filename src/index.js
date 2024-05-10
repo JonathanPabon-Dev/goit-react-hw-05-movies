cf7a6efd1e6fbee464a4acb930582b0cb2894e2a
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-05-10 09:32:35 -0500

    fix: routing use HashRouter

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'modern-normalize';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
